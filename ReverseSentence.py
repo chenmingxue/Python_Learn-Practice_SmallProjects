7e3be5d4ff93f9ccb19ea34ef5cec28d3b0c2dea
ChenmingXue <chenming.xue2011@gmail.com>
2019-05-20 23:18:28 -0700

    Update ReverseSentence.py

--- ReverseSentence.py
+++ ReverseSentence.py
@@ -1,1 +1,13 @@
-class 
+class ReverseSentence:
+  def __init__(self, sentence = "Hello World"):
+    self.sentence = sentence
+
+  def reverse(self):
+    self.words = self.sentence.split(" ")
+    return " ".join(self.words[::-1])
+
+
+if __name__ == "__main__":
+  sentence=input("Please write your sentence: ")
+  reverse_sentence=ReverseSentence(sentence)
+  print("The reversed sentence is: ", reverse_sentence.reverse())
