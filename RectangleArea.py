96e5eb6d5ac5ca8643696ab4e27fbf5ca4cd4dc1
ChenmingXue <chenming.xue2011@gmail.com>
2019-05-22 18:56:54 -0700

    Update RectangleArea.py

--- RectangleArea.py
+++ RectangleArea.py
@@ -1,1 +1,21 @@
+#Write a Python class named Rectangle constructed by a length and width and a method which will compute
+#the area of a rectangle. - Go to the editor
+class RectangleArea:
+    def __init__(self):
+        self.length = 0
+        self.width = 0
 
+    def get_parameters(self, input_length, input_width):
+        self.length = float(input_length)
+        self.width = float(input_width)
+
+    def calculate_area(self):
+        return self.length*self.width
+
+
+if __name__ == "__main__":
+    input_length = input("Please key in the length: ")
+    input_width = input("Please key in the width: ")
+    rectangle = RectangleArea()
+    rectangle.get_parameters(input_length, input_width)
+    print (rectangle.calculate_area())
