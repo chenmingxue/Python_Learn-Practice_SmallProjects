c0396c7619661121f44642666e94e0f827f64f27
Samuel Yap <samuelywp@gmail.com>
2019-05-23 14:16:35 +0800

    Update Encapsulation_inheritance.py
    Hey man, good work you have done but read up more on how Python should be styled.
    E.g. Class names should start off with a capital letter, and any additional words append it directly starting with a capital. Like AwesomeClass and not Awesome_Class or Awesome_class
    Also, there should be spacing before and after = signs, unless you are declaring default parameters e.g. default_parameter=somevalue or key - value pairs in dictionary, both if following the convention no spacing before and after = sign
    https://stackoverflow.com/questions/159720/what-is-the-naming-convention-in-python-for-variable-and-function-names

--- Encapsulation_inheritance.py
+++ Encapsulation_inheritance.py
@@ -1,48 +1,48 @@
 #example of encapsulation 1
 class Person:
-    def __init__(self, first_name, email):
-        self.first_name = first_name #public variable
-        self._email = email   #private variable, use for encapsulation: the private variable is used only internally
+    def __init__(self, fname, email):
+        self.first_name = fname #public variable
+        self.email = email   #private variable, use for encapsulation: the private variable is used only internally
 
-    def update_emai(self, new_email):
-        self._email = new_email
+    def update_email(self, email):
+        self.email = email
 
     def email(self):
-        return self._email
+        return self.email
 
 tk = Person('tk','tk@email')
-print(tk.first_name, tk.email())
-tk.update_emai('new@email')
+print(tk.fname, tk.email())
+tk.update_email('new@email')
 print(tk.email())
 
 #example of encapsulation 2
 class Person:
-    def __init__(self, first_name, age):
+    def __init__(self, fname, age):
         self.first_name = first_name
-        self._age = age
+        self.age = age
 
     def show_age(self):
-        return self._get_age()
+        return self.get_age()
 
-    def _get_age(self):
-        return self._age
+    def get_age(self):
+        return self.age
 
 tk = Person('tk', 20)
 print(tk.show_age())
 
 #example of inheritance
 class Car:
-    def __init__(self, number_of_wheels, seating_capacity, maximum_velocity):
-        self.number_of_wheels = number_of_wheels
+    def __init__(self, num_of_wheels, seating_capacity, max_velocity):
+        self.num_of_wheels = num_of_wheels
         self.seating_capacity = seating_capacity
-        self.maximum_velocity = maximum_velocity
+        self.max_velocity = max_velocity
 
     def make_noise(self):
         print("YeeHaaaa!")
 
-class Electric_car(Car):
-    def __init__(self, number_of_wheels, seating_capacity, maximum_velocity, battery):
-        Car.__init__(self, number_of_wheels, seating_capacity, maximum_velocity)
+class ElectricCar(Car):
+    def __init__(self, num_of_wheels, seating_capacity, max_velocity, battery):
+        Car.__init__(self, num_of_wheels, seating_capacity, max_velocity)
         self.battery = battery
         self.state = ''
 
@@ -50,9 +50,9 @@ class Electric_car(Car):
         self.state = sold_state
 
 
-electric_car=Electric_car(4, 5, 250, "1000V")
+electric_car = ElectricCar(4, 5, 250, "1000V")
 electric_car.sold_in_state("CA")
 electric_car.make_noise()
-print(electric_car.number_of_wheels)
+print(electric_car.num_of_wheels)
 print(electric_car.battery)
 print(electric_car.state)
